79287b1d9d2cdd3d3ae6692b1b5886dca50a0d6b
kaveenr <u.k.k.rodrigo@gmail.com>
2014-04-14 01:25:07 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the Timer Application written for Toastmasters which will be later linked to a tri color light which show the time limit color via Bluetooth
---
+++ main.js
@@ -0,0 +1,45 @@
+//main.js
+var seconds =0; //timer counter
+var timer = null; //global variable for timer
+// Timing Circuits
+function tick(total,green,orange){
+	seconds += 1;
+	$("#timecode").html(read_sec(seconds));
+	console.log(seconds);
+	switch (seconds){
+		case green:
+			$("#page").attr("style","background-color:#0C0");
+			break;
+		case orange:
+			$("#page").attr("style","background-color:#FF9900");
+			break;
+		case total:
+			$("#page").attr("style","background-color:#FF0000");
+			break;
+	}
+}
+//seconds to reaable
+function read_sec(src){
+	var minutes = Math.floor(src / 60);
+	var seconds = (src - minutes*60)
+	return minutes+" : "+seconds;
+}
+//Timer functions
+function startTimer(total,green,orange){
+	$("#page").attr("style","background-color:");
+	if (seconds != null){
+		stopTimer();
+	}
+	seconds =0;
+	timer = setInterval(function(){
+		tick(total,green,orange);
+	},1000);	
+}
+
+function stopTimer(){
+	clearInterval(timer);
+	timer = null;
+}
+//onLoad
+$(document).ready(function(e) {
+});
